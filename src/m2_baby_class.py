c83f17ee37b4151a6a1f978d8ae6c12348f12336
Joseph Law <lawjk@rose-hulman.edu>
2018-12-17 13:10:39 -0500

    DONE

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Joseph Law.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -40,9 +40,24 @@ def main():
         for j in range(4):
             mckinley.hour_passes()
 
-
+class Baby(object):
+    def __init__(self,name):
+        self.name = name
+        print('Hello baby ' + self.name + '!')
+        self.hour = 0
+    def feed_baby(self):
+        print('Thank you for feeding baby ' + self.name)
+        self.hour = 0
+    def hour_passes(self):
+        if self.hour == 0:
+            print('Baby' + self.name + ' is sleeping.')
+        elif self.hour == 1:
+            print('Baby' + self.name + ' is awake.  Time for food.')
+        else:
+            print('Baby' + self.name + 'is CRYING uncontrollably!  Feed the Baby!')
+        self.hour = self.hour + 1
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  Step 2a:  Implement a class called   Baby   that has a constructor method
 #            (__init__) and two other methods, as described below.
